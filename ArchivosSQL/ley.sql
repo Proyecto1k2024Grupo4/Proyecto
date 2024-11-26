4d4461aa46d9a60c9026d8219ce0bea66d7b71ed
diego <diegofjda2@gmail.com>
2024-11-26 16:46:05 +0100

    proceso ley

---
+++ ArchivosSQL/ley.sql
@@ -0,0 +1,11 @@
+CREATE TABLE LEY(
+    id INT UNSIGNED,
+    descripcion VARCHAR(2000) NOT NULL,
+    fechaAplicacion DATE,
+    fechaModificacion DATE,
+    fechaImplementacion DATE,
+    idCodigoCivil INT UNSIGNED,
+    CONSTRAINT PK_LEY PRIMARY KEY (id),
+    CONSTRAINT FK_LEY_CODIGO_CIVIL FOREIGN KEY (idCodigoCivil) REFERENCES CODIGO_CIVIL(id)
+
+);
