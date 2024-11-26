2e22609617308fb6bf3d81a85d238951f4abdf47
diego <diegofjda2@gmail.com>
2024-11-26 13:04:29 +0100

    terminado pais y persona

--- ArchivosSQL/persona.sql
+++ ArchivosSQL/persona.sql
@@ -1,3 +1,11 @@
 CREATE TABLE PERSONA(
-    nombre VARCHAR()
-);
+    numPasaporte VARCHAR(16),
+    nombre VARCHAR(32) NOT NULL,
+    primerApellido VARCHAR(32),
+    segundoApellido VARCHAR(32),
+    fnac DATE,
+    sexo CHAR(1),
+    paisNacimiento VARCHAR(64) NOT NULL,
+    CONSTRAINT PK_PERSONA PRIMARY KEY (numPasaporte),
+    CONSTRAINT FK_PERSONA_PAIS FOREIGN KEY (paisNacimiento) REFERENCES PAIS(nombre)
+ );
