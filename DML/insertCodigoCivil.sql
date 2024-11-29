3eab5ea2d5a444ab4d8e821ddf1235fff7c32169
diego <diegofjda2@gmail.com>
2024-11-29 09:06:02 +0100

    insertCodigoCivil y insertCongreso

---
+++ DML/insertCodigoCivil.sql
@@ -0,0 +1,11 @@
+INSERT INTO CODIGO_CIVIL(nombrePais) VALUES 
+("España"),
+("Colombia"),
+("Estados Unidos"),
+("Rusia"),
+("China"),
+("Mongolia"),
+("Italia"),
+("Grecia"),
+("Francia"),
+("Reino Unido");
---
+++ DML/insertCongreso.sql
@@ -0,0 +1,11 @@
+INSERT INTO CONGRESO(nombrePais) VALUES 
+("España"),
+("Colombia"),
+("Estados Unidos"),
+("Rusia"),
+("China"),
+("Mongolia"),
+("Italia"),
+("Grecia"),
+("Francia"),
+("Reino Unido");
