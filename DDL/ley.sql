0c8b74d64f8089a942fce27dab1e124b4aa82151
diego <diegofjda2@gmail.com>
2024-12-01 13:02:41 +0100

    guardar

--- DDL/ley.sql
+++ DDL/ley.sql
@@ -1,13 +1,12 @@
 CREATE TABLE LEY(
-    id INT UNSIGNED,
+    id INT UNSIGNED AUTO_INCREMENT,
     descripcion VARCHAR(2000) NOT NULL,
     fechaAplicacion DATE,
     fechaModificacion DATE,
-    fechaImplementacion DATE,
-    idCodigoCivil INT UNSIGNED,
+    fechaImplementacion DATE NOT NULL,
+    idCodigoCivil INT UNSIGNED NOT NULL,
     CONSTRAINT PK_LEY PRIMARY KEY (id),
     CONSTRAINT FK_LEY_CODIGO_CIVIL FOREIGN KEY (idCodigoCivil) REFERENCES CODIGO_CIVIL(id)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION
-
 );
