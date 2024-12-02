e04ddbfce99dea1718b249d6146a6af314e41661
diego <diegofjda2@gmail.com>
2024-12-02 09:22:27 +0100

    guardar

---
+++ DML/ScriptsPorSeparado/insertPersona.sql
@@ -0,0 +1,35 @@
+INSERT INTO PERSONA(numPasaporte, nombre, primerApellido, segundoApellido, fnac, sexo, paisNacimiento) VALUES
+("123456789A", "Desiderio", "Alcazares", "Segundo", '2000/1/10', 'H', "España"),
+("234567891A", "Juan", "Pericles", NULL, '1900/2/10', 'H', "España"),
+("12345678B", "Lucia", NULL, "Artida", '1995/3/10', 'M', "Colombia"),
+("23456789B", "Cullera", "Macia", "Roberto", '2006/4/25', 'H', "Colombia"),
+("A1234567C", "Wilson", "Jackson", "Person", '2001/5/1', 'H', "Estados Unidos"),
+("A2345678C", "Tracy", "Mary", "Larson", '1999/5/10', 'M', "Estados Unidos"),
+("12345678901D", "Bladimyr", "Bostov", "Artyom", '1997/6/15', 'H', "Rusia"),
+("12345678901D", "Marie", "Azkaya", "Ligla", '1980/9/15', 'M', "Rusia"),
+("12345E", "Xi", "Ginpin", "Tzu", '1997/6/15', 'H', "China"),
+("23456E", "Xensue", "Laocao", "Lu", '2002/8/17', 'H', "China"),
+("123456789F", "Gengis", "Khan", "Yangau", '2002/9/17', NULL, "Mongolia"),
+("234567891F", "Curimi", "Bukyak", "Nica", '1980/9/13', 'H', "Mongolia"),
+("123G", "Pesto", "Di", "Cuesto", '1998/10/6', 'H', "Italia"),
+("234G", "Carabina", "Na", "Politana", '1990/10/26', 'M', "Italia"),
+("1234567890123H", "Yikmir", "Amir", "Palir", '1980/11/30', 'H', "Grecia"),
+("2345678901234H", "Bracut", "Nagal", "Sinsi", '1960/1/30', 'H', "Grecia"),
+("123456I", "Bagguette", "Croisant", "Eifel", '1999/12/1', 'H', "Francia"),
+("234567I", "Wi", "Wi", "Lacamoua", '2003/12/2', 'M', "Francia"),
+("12345678J", "Charles", "Darwin", "Natural", '1950/2/2', 'H', "Reino Unido"),
+("12345678J", "Isabel", "The", "Queen", '1200/1/25', 'M', "Reino Unido");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
