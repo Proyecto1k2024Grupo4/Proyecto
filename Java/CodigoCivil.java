664ffc55059930f1e8747a1865f7d5e69d4406a0
Villalba <hola@hola.com>
2025-01-16 20:07:49 +0100

    Diego es tonto(esto es verdad)

--- Java/CodigoCivil.java
+++ Java/CodigoCivil.java
@@ -2,9 +2,6 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
 public class CodigoCivil {
 
     /**
@@ -13,14 +10,35 @@ public class CodigoCivil {
     public CodigoCivil() {
     }
 
-    /**
-     * 
-     */
-    public void id;
+    private int id;
+    private int idPais;
 
-    /**
-     * 
-     */
-    public void idPais;
+    public String toJson(){
+        StringBuilder jsonBuilder = new StringBuilder();
+
+        jsonBuilder.append("\"CODIGO_CIVIL\": {")
+                .append("\n")
+                .append("\t")
+                .append("\"id\": ").append(this.id)
+                .append("\n")
+                .append("\t")
+                .append("\"idPais\": ").append(this.idPais)
+                .append("\n")
+                .append("}");
+        return String.valueOf(jsonBuilder);
+    }
+
+    public String toXml(){
+        StringBuilder xmlBuilder = new StringBuilder();
+
+        xmlBuilder.append("<CodigoCivil>")
+                .append("\n")
+                .append("<CodigoCivil:id>").append(this.id).append("</CodigoCivil:id>\n")
+                .append("\t")
+                .append("<CodigoCivil:idPais>").append(this.idPais).append("</CodigoCivil:idPais>\n")
+                .append("</CodigoCivil");
+
+        return String.valueOf(xmlBuilder);
+    }
 
 }
--- Java/Ley.java
+++ Java/Ley.java
@@ -2,9 +2,7 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * 
- */
+
 public class Ley {
 
     /**
@@ -13,34 +11,57 @@ public class Ley {
     public Ley() {
     }
 
-    /**
-     * 
-     */
-    public void id;
 
-    /**
-     * 
-     */
-    public void descripcion;
+    private int id;
+    private String descripcion;
+    private String fechaAplicacion;
+    private String fechaModificacion;
+    private String fechaImplementacion;
+    private int idCodigoCivil;
 
-    /**
-     * 
-     */
-    public void fechaAplicacion;
+    public String toJson(){
+        StringBuilder jsonBuilder = new StringBuilder();
+        jsonBuilder.append("\"LEY\": {\n")
+                .append("\t")
+                .append("\"id\": ").append(this.id)
+                .append("\n")
+                .append("\t")
+                .append("\"descripcion\": \"").append(this.descripcion).append("\"")
+                .append("\n")
+                .append("\t")
+                .append("\"fechaAplicacion\": \"").append(this.fechaAplicacion).append("\"")
+                .append("\n")
+                .append("\t")
+                .append("\"fechaModificacion\": \"").append(this.fechaModificacion).append("\"")
+                .append("\n")
+                .append("\t")
+                .append("\"fechaImplementacion\": \"").append(this.fechaImplementacion).append("\"")
+                .append("\n")
+                .append("\t")
+                .append("\"idCodigoCivil: ").append(this.idCodigoCivil)
+                .append("\n")
+                .append("}");
+        return String.valueOf(jsonBuilder);
+    }
 
-    /**
-     * 
-     */
-    public void fechaModificacion;
+    public String toXml(){
+        StringBuilder xmlBuilder = new StringBuilder();
+        xmlBuilder.append("<Ley>\n")
+                .append("\t")
+                .append("<Ley:id>").append(this.id).append("</Ley:id>\n")
+                .append("\t")
+                .append("<Ley:descripcion>").append(this.descripcion).append("</Ley:descripcion>\n")
+                .append("\t")
+                .append("<Ley:fechaAplicacion>").append(this.fechaAplicacion).append("<Ley:fechaAplicacion\n")
+                .append("\t")
+                .append("<Ley:fechaModificacion").append(this.fechaModificacion).append("</Ley:fechaModificacion>\n")
+                .append("\t")
+                .append("<Ley:fechaImplementacion").append(this.fechaImplementacion).append("</Ley:fechaImplementacion\n")
+                .append("\t")
+                .append("Ley:idCodigoCivil").append(this.idCodigoCivil).append("</Ley:idCodigoCivil")
+                .append("</Ley");
 
-    /**
-     * 
-     */
-    public void fechaImplementacion;
-
-    /**
-     * 
-     */
-    public void idCodigoCivil;
+        return String.valueOf(xmlBuilder);
+    }
 
 }
