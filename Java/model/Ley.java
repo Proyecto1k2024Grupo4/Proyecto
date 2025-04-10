fe81d87fc76073d09ffd2a170f15ffa87aaf50ff
diego <diegofjda2@gmail.com>
2025-04-10 17:49:43 +0200

    Guardar

--- Java/model/Ley.java
+++ Java/model/Ley.java
@@ -1,8 +1,11 @@
 package model;
 
 
-import java.util.Date;
-
+/**
+ * Clase Ley que sirve para almacenar la información de las leyes
+ * @author Diego Fernando Valencia Correa 1ºK
+ * @version 7-4-2025
+ */
 public class Ley {
 
     // Propiedades
@@ -15,18 +18,12 @@ public class Ley {
 
 
     /**
-     * Default constructor
-     */
-    public Ley() {
-    }
-
-    /**
-     * Constructor sin ID
-     * @param descripcion
-     * @param fechaAplicacion
-     * @param fechaModificacion
-     * @param fechaImplementacion
-     * @param idCodigoCivil
+     * Constructor de Ley con todos los parámetros menos el ID
+     * @param descripcion Descripción de la ley
+     * @param fechaAplicacion Fecha de apliación de la ley
+     * @param fechaModificacion Fecha de modificación de la ley
+     * @param fechaImplementacion Fecha de implementación de la ley
+     * @param idCodigoCivil Id del código civil de la ley
      */
     public Ley(String descripcion, java.sql.Date fechaAplicacion, java.sql.Date fechaModificacion, java.sql.Date fechaImplementacion, int idCodigoCivil) {
         setDescripcion(descripcion);
@@ -37,13 +34,13 @@ public class Ley {
     }
 
     /**
-     * Constructor con todos los parametros
-     * @param id
-     * @param descripcion
-     * @param fechaAplicacion
-     * @param fechaModificacion
-     * @param fechaImplementacion
-     * @param idCodigoCivil
+     * Constructor de Ley con todos los parámetros
+     * @param id Id de la ley
+     * @param descripcion Descripción de la ley
+     * @param fechaAplicacion Fecha de apliación de la ley
+     * @param fechaModificacion Fecha de modificación de la ley
+     * @param fechaImplementacion Fecha de implementación de la ley
+     * @param idCodigoCivil Id del código civil de la ley
      */
     public Ley(int id, String descripcion, java.sql.Date fechaAplicacion, java.sql.Date fechaModificacion, java.sql.Date fechaImplementacion, int idCodigoCivil) {
         this.id = id;
@@ -54,6 +51,10 @@ public class Ley {
         setIdCodigoCivil(idCodigoCivil);
     }
 
+    /**
+     * Metodo que tranforma la información de una ley en formato JSON
+     * @return JSON con la información
+     */
     public String toJson(){
         StringBuilder jsonBuilder = new StringBuilder();
         jsonBuilder.append("\"LEY\": {\n")
@@ -79,6 +80,10 @@ public class Ley {
         return String.valueOf(jsonBuilder);
     }
 
+    /**
+     * Metodo que transorma la información de una ley en formato XML
+     * @return XML con la información de la ley
+     */
     public String toXml(){
         StringBuilder xmlBuilder = new StringBuilder();
         xmlBuilder.append("<model.Ley>\n")
@@ -99,46 +104,90 @@ public class Ley {
         return String.valueOf(xmlBuilder);
     }
 
+    /**
+     * Metodo que devuelve el id de la ley
+     * @return Id de la ley
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Metodo que devuelve la descripción de la ley
+     * @return String con la descripción
+     */
     public String getDescripcion() {
         return descripcion;
     }
 
+    /**
+     * Metodo que establece la descripción de la ley
+     * @param descripcion Descripción de la ley
+     */
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
 
+    /**
+     * Metodo que devuelve la fecha de aplicación de la ley
+     * @return Fecha de aplicación
+     */
     public java.sql.Date getFechaAplicacion() {
         return fechaAplicacion;
     }
 
+    /**
+     * Metodo que establece la fecha de aplicación de la ley
+     * @param fechaAplicacion Fecha de aplicación
+     */
     public void setFechaAplicacion(java.sql.Date fechaAplicacion) {
         this.fechaAplicacion = fechaAplicacion;
     }
 
+    /**
+     * Metodo que devuelve la fecha de modificación de la ley
+     * @return Fecha de modificación
+     */
     public java.sql.Date getFechaModificacion() {
         return fechaModificacion;
     }
 
+    /**
+     * Metodo que establece la fecha de modificación de la ley
+     * @param fechaModificacion Fecha de modificación
+     */
     public void setFechaModificacion(java.sql.Date fechaModificacion) {
         this.fechaModificacion = fechaModificacion;
     }
 
+    /**
+     * Metodo que devuelve la fecha de implementación de la ley
+     * @return Fecha de implementación de la ley
+     */
     public java.sql.Date getFechaImplementacion() {
         return fechaImplementacion;
     }
 
+    /**
+     * Metodo que establece la fecha de implementación de la ley
+     * @param fechaImplementacion Fecha de implementación de la ley
+     */
     public void setFechaImplementacion(java.sql.Date fechaImplementacion) {
         this.fechaImplementacion = fechaImplementacion;
     }
 
+    /**
+     * Metodo que devuelve el id del código civil relacionado con la ley
+     * @return int Id del código civil
+     */
     public int getIdCodigoCivil() {
         return idCodigoCivil;
     }
 
+    /**
+     * Metodo que establece el id del código civil relacionado con la ley
+     * @param idCodigoCivil Id del código civil
+     */
     public void setIdCodigoCivil(int idCodigoCivil) {
         this.idCodigoCivil = idCodigoCivil;
     }
--- Java/model/Sexo.java
+++ Java/model/Sexo.java
@@ -1,5 +1,10 @@
 package model;
 
+/**
+ * Enum Sexo que almacena los posibles valores de género para las personas
+ * @author Diego Fernando Valencia Correa | Samini Abdel | Jonathan Villalba
+ * @version 10-4-2025
+ */
 public enum Sexo {
     H, M
 }
